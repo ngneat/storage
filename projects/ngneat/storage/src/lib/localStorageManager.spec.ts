a0a7be422db7998453b96e8694b8cd0188769a34
va-stefanek <mateusz.stefanczyk@valueadd.pl>
2021-05-30 10:46:14 +0200

    feat(app): add jest

--- projects/ngneat/storage/src/lib/localStorageManager.spec.ts
+++ projects/ngneat/storage/src/lib/localStorageManager.spec.ts
@@ -13,20 +13,20 @@ const createStorageManager = () => {
 };
 
 describe('LocalStorageManager', () => {
-  // it('should set value', () => {
-  //   const person = { name: 'ewan', phone: { num: 5550153, prefix: 288 } };
-  //   const spy = jest.spyOn(localStorage, 'setItem').mockImplementation(() => {});
-  //   const storageManager = createStorageManager();
-  //   const response = storageManager.setValue('key', person);
-  //   expect(spy).toHaveBeenCalledWith('key', JSON.stringify(person));
-  //   expect(response).toEqual(person);
-  // });
-  //
-  // it('should get value', () => {
-  //   const spy = jest.spyOn(localStorage, 'getItem').mockImplementation(() => '{ "name": "ewan" }');
-  //   const storageManager = createStorageManager();
-  //   const value = storageManager.getValue('key');
-  //   expect(spy).toHaveBeenCalledWith('key');
-  //   expect(value).toEqual({ name: 'ewan' });
-  // });
+  it('should set value', () => {
+    const person = { name: 'ewan', phone: { num: 5550153, prefix: 288 } };
+    const spy = jest.spyOn(localStorage, 'setItem').mockImplementation(() => {});
+    const storageManager = createStorageManager();
+    const response = storageManager.setValue('key', person);
+    expect(spy).toHaveBeenCalledWith('key', JSON.stringify(person));
+    expect(response).toEqual(person);
+  });
+
+  it('should get value', () => {
+    const spy = jest.spyOn(localStorage, 'getItem').mockImplementation(() => '{ "name": "ewan" }');
+    const storageManager = createStorageManager();
+    const value = storageManager.getValue('key');
+    expect(spy).toHaveBeenCalledWith('key');
+    expect(value).toEqual({ name: 'ewan' });
+  });
 });
